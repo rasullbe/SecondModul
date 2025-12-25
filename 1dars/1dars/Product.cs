8914efac13e61cbf0507128b862a49795e596910
rasullbe <rasulbek112208@gmail.com>
2025-12-25 21:04:52 +0500

    homework

---
+++ 1dars/1dars/Product.cs
@@ -0,0 +1,17 @@
+﻿namespace _1dars;
+
+public class Product
+{
+    public Guid GuidOfProduct { get; set; }
+    public string ProductName { get; set; }
+    public int Price { get; set; }
+    public DateTime date { get; set; }
+    public string Description { get; set; }
+
+
+
+    override public string ToString()
+    {
+        return $"Guid: {GuidOfProduct}\nName: {ProductName}\nPrice: {Price}\nDate: {date}\nDescription: {Description}\n";
+    }
+}
--- 1dars/1dars/Program.cs
+++ 1dars/1dars/Program.cs
@@ -4,6 +4,15 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Product product = new Product()
+        {
+            GuidOfProduct = Guid.NewGuid(),
+            ProductName = "Laptop",
+            Price = 1500,
+            date = DateTime.Now,
+            Description = "A high-performance laptop suitable for gaming and professional work."
+
+        };
+        Console.WriteLine(product);
     }
 }
