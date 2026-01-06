2c440810bff5d5b4d91e1cd6625eb055184c45c8
rasullbe <rasulbek112208@gmail.com>
2026-01-06 16:59:55 +0500

    homework

---
+++ 2dars/2dars/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace _2dars.Models;
+
+public class Book
+{
+    public Guid BookId { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public decimal Price { get; set; }
+    public string Description { get; set; }
+    public DateTime Created { get; set; }
+    public string Genre { get; set; }
+    public int PageCount { get; set; }
+}
---
+++ 2dars/2dars/Program.cs
@@ -0,0 +1,83 @@
+﻿using _2dars.Models;
+namespace _2dars;
+
+internal class Program
+{
+    static List<Book> books = new List<Book>();
+    static void Main(string[] args)
+    {
+        ListSeed();
+        var pages = GetMaxPageBook();
+        Console.WriteLine(pages.PageCount);
+        //var books = GetMostPage(100);
+    }
+
+    static List<Book> GetMostPage(int minPage)
+    {
+        var LotsPagesBook = new List<Book>();
+
+        foreach (var book in books)
+        {
+            if (book.PageCount > minPage)
+            {
+                LotsPagesBook.Add(book);
+            }
+        }
+
+        return LotsPagesBook;
+    }
+
+    static Book GetMaxPageBook()
+    {
+        Book book = books[0];
+
+        foreach (var p in books)
+        {
+            if (book.PageCount < p.PageCount)
+            {
+                book = p;
+            }
+        }
+
+        return book;
+    }
+
+    static void ListSeed()
+    {
+        books.Add(new Book()
+        {
+            BookId = Guid.NewGuid(),
+            Title = "The Great Gatsby",
+            Author = "F. Scott Fitzgerald",
+            Price = 10.99m,
+            Description = "A novel set in the Jazz Age that tells the story of Jay Gatsby's unrequited love for Daisy Buchanan.",
+            Created = DateTime.Now,
+            Genre = "Classic",
+            PageCount = 180
+        });
+
+        books.Add(new Book()
+        {
+            BookId = Guid.NewGuid(),
+            Title = "Sariq Devni Minib",
+            Author = "Xudoyberdi To'xtaboyev",
+            Price = 5.99m,
+            Description = "Legenda",
+            Created = DateTime.Now,
+            Genre = "Fantasy",
+            PageCount = 270
+        });
+
+        books.Add(new Book()
+        {
+            BookId = Guid.NewGuid(),
+            Title = "Harry Poter",
+            Author = "Opa",
+            Price = 15.99m,
+            Description = "Legenda 2",
+            Created = DateTime.Now,
+            Genre = "Fantasy",
+            PageCount = 870
+        });
+    }
+}
