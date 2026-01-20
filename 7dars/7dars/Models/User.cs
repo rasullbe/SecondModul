5039ed4109e75f42f5f1813aabaf87c0ab217192
rasullbe <rasulbek112208@gmail.com>
2026-01-20 22:36:10 +0500

    homework

---
+++ 7dars/7dars/Models/User.cs
@@ -0,0 +1,53 @@
+﻿namespace _7dars.Models;
+
+public class User
+{
+	private Guid userID;
+	public Guid userId
+	{
+		get { return userID; }
+		set { userID = value; }
+	}
+
+	private string Username;
+	public string UserName
+	{
+		get { return Username; }
+		set { Username = value; }
+	}
+
+	private string FirstName;
+	public string firstName
+	{
+		get { return FirstName; }
+		set { FirstName = value; }
+    }
+
+	private string LastName;
+	public string lastName
+	{
+		get { return LastName; }
+		set { LastName = value; }
+    }
+
+    private string Password;
+	public string password
+	{
+		get { return Password; }
+		set { Password = value; }
+    }
+
+	private string Email;
+	public string email
+	{
+		get { return Email; }
+		set { Email = value; }
+    }
+
+	private string PhoneNumber;
+	public string phoneNumber
+	{
+		get { return PhoneNumber; }
+		set { PhoneNumber = value; }
+    }
+}
---
+++ 7dars/7dars/Program.cs
@@ -0,0 +1,20 @@
+﻿using _7dars.Models;
+
+namespace _7dars;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        User user = new User()
+        {
+            userId = Guid.NewGuid(),
+            UserName = "john_doe",
+            firstName = "John",
+            lastName = "Doe",
+            password = "securepassword123",
+            email = "johndoeisagoodguy@gmail.com",
+            phoneNumber = "+1234567890"
+        };
+    }
+}
